c397f108b4ce00c192b1ba5d6715dcb897592a12
lars.francke <lars.francke@gmail.com>
2011-10-19 16:01:22 +0000

    Add some logging.

--- selenium-test/trunk/src/test/java/org/gbif/SeleniumIT.java
+++ selenium-test/trunk/src/test/java/org/gbif/SeleniumIT.java
@@ -16,12 +16,15 @@ public class SeleniumIT {
 
   @Before
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    System.out.println("Setting up driver");
+    driver = new LoggingFirefoxDriver();
+    System.out.println("Driver set up");
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testFoo() throws Exception {
+    System.out.println("Getting URL");
     driver.get(baseUrl);
     driver.findElement(By.linkText("Datasets")).click();
     driver.findElement(By.cssSelector("button.search_button")).click();
