fb25eef91cf1be993c6a47fea277c45a4caece95
josecuadra <josecuadra@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-03-13 14:12:34 +0000

    [registry-api] all changes to the API project

---
+++ registry2/trunk/registry-api/src/main/java/org/gbif/api/registry/vocabulary/IdentifierType.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.api.registry.vocabulary;
+
+import org.gbif.api.util.VocabularyUtils;
+
+/**
+ * Enumeration for all possible identifier types.
+ */
+public enum IdentifierType {
+
+  SOURCE_ID,
+  URL,
+  LSID,
+  HANDLER,
+  DOI,
+  UUID,
+  FTP,
+  URI,
+  UNKNOWN,
+  GBIF_PORTAL,
+  GBIF_NODE;
+
+  /**
+   * @param identifier type
+   *
+   * @return the matching {@link IdentifierType} or null
+   */
+  public static IdentifierType fromString(String identifierType) {
+    return (IdentifierType) VocabularyUtils.lookupEnum(identifierType, IdentifierType.class);
+  }
+}
