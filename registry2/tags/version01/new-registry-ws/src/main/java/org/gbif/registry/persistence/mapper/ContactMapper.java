52bb147e640d3d8dfd282fb1f452b90dd82388ef
josecuadra <josecuadra@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-03-13 14:15:44 +0000

    deleting wrong tag

--- registry2/tags/version01/new-registry-ws/src/main/java/org/gbif/registry/persistence/mapper/ContactMapper.java
+++
@@ -1,9 +0,0 @@
-package org.gbif.registry.persistence.mapper;
-
-import org.gbif.api.registry.model.Contact;
-
-
-public interface ContactMapper {
-
-  int createContact(Contact contact);
-}
--- registry2/tags/version01/new-registry-ws/src/main/java/org/gbif/registry/persistence/mapper/InstallationMapper.java
+++
@@ -1,8 +0,0 @@
-package org.gbif.registry.persistence.mapper;
-
-import org.gbif.api.registry.model.Installation;
-
-public interface InstallationMapper extends NetworkEntityMapper<Installation>, ContactableMapper, AccessibleMapper,
-  MachineTaggableMapper, TaggableMapper, CommentableMapper {
-
-}
