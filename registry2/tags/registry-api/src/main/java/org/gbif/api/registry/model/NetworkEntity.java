11c30061723b1d6d8dc0f1a1c712047086f4eb9f
josecuadra <josecuadra@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-03-13 13:12:35 +0000

    tagging registry-api

---
+++ registry2/tags/registry-api/src/main/java/org/gbif/api/registry/model/NetworkEntity.java
@@ -0,0 +1,44 @@
+package org.gbif.api.registry.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
+/**
+ * This interface provides a minimal contract that all network entities (The readable version) will adhere to. It is
+ * used <em>only</em> to simplify consistent testing of operations on network entities, hence the restriction to package
+ * visibility only.
+ */
+public interface NetworkEntity {
+
+  @Nullable
+  public UUID getKey();
+
+  public void setKey(UUID key);
+
+  @Nullable
+  String getTitle();
+
+  void setTitle(String title);
+
+  @Nullable
+  String getDescription();
+
+  void setDescription(String description);
+
+  @Nullable
+  Date getCreated();
+
+  void setCreated(Date created);
+
+  @Nullable
+  Date getModified();
+
+  void setModified(Date modified);
+
+  @Nullable
+  Date getDeleted();
+
+  void setDeleted(Date deleted);
+}
