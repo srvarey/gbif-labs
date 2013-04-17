e353410d537ec96daefeb5040fee705758abdd42
timrobertson100@gmail.com <timrobertson100@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-04-17 13:07:32 +0000

    Postgres 8.4 does not have an optimized truncate statement (9.2+) so reverts to deletion.
    On an empty DB, this is the difference:

--- registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
+++ registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
@@ -21,6 +21,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
 import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,7 @@ import org.slf4j.LoggerFactory;
 /**
  * A Rule that will truncate the tables ready for a new test. It is expected to do this before each test by using the
  * following:
- *
+ * 
  * <pre>
  * @Rule
  * public DatabaseInitializer = new DatabaseInitializer(getDatasource()); // developer required to provide datasource
@@ -38,6 +39,49 @@ public class DatabaseInitializer extends ExternalResource {
 
   private static final Logger LOG = LoggerFactory.getLogger(DatabaseInitializer.class);
   private final DataSource dataSource;
+
+  private static final ImmutableList<String> TABLES = ImmutableList.of(
+    "node",
+    "node_contact",
+    "node_machine_tag",
+    "node_tag",
+    "node_comment",
+    "organization",
+    "organization_contact",
+    "organization_endpoint",
+    "organization_machine_tag",
+    "organization_tag",
+    "organization_identifier",
+    "organization_comment",
+    "installation",
+    "installation_contact",
+    "installation_endpoint",
+    "installation_machine_tag",
+    "installation_tag",
+    "installation_comment",
+    "dataset",
+    "dataset_contact",
+    "dataset_endpoint",
+    "dataset_machine_tag",
+    "dataset_tag",
+    "dataset_identifier",
+    "dataset_comment",
+    "dataset_network",
+    "network",
+    "network_contact",
+    "network_endpoint",
+    "network_machine_tag",
+    "network_tag",
+    "network_comment",
+    "contact",
+    "endpoint",
+    "endpoint_machine_tag",
+    "machine_tag",
+    "tag",
+    "identifier",
+    "comment",
+    "metadata"
+    );
 
   public DatabaseInitializer(DataSource dataSource) {
     this.dataSource = dataSource;
@@ -49,16 +93,10 @@ public class DatabaseInitializer extends ExternalResource {
     Connection connection = dataSource.getConnection();
     try {
       connection.setAutoCommit(true);
-      connection.createStatement().execute("TRUNCATE TABLE " +
-        "node, node_contact, node_machine_tag, node_tag, node_comment, " +
-        "organization, organization_contact, organization_endpoint, organization_machine_tag, " +
-        "organization_tag, organization_identifier, organization_comment, " +
-        "installation, installation_contact, installation_endpoint, installation_machine_tag, " +
-        "installation_tag, installation_comment, " +
-        "dataset, dataset_contact, dataset_endpoint, dataset_machine_tag, dataset_tag, " +
-        "dataset_identifier, dataset_comment, " +
-        "network, network_contact, network_endpoint, network_machine_tag, network_tag, network_comment, " +
-        "contact, endpoint, machine_tag, tag, identifier, comment CASCADE");
+      for (String table : TABLES) {
+        connection.createStatement().execute("DELETE FROM TABLE " + table + " CASCADE");
+      }
+
     } catch (SQLException e) {
       Throwables.propagate(e);
     } finally {
