dfe085a09aba8fd4b84e456e34379d93f984fe10
timrobertson100@gmail.com <timrobertson100@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-04-17 13:29:12 +0000

    Reverting truncate statement

--- registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
+++ registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2013 Global Biodiversity Information Facility (GBIF)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +18,6 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableList;
 import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,49 +36,6 @@ public class DatabaseInitializer extends ExternalResource {
   private static final Logger LOG = LoggerFactory.getLogger(DatabaseInitializer.class);
   private final DataSource dataSource;
 
-  private static final ImmutableList<String> TABLES = ImmutableList.of(
-    "node",
-    "node_contact",
-    "node_machine_tag",
-    "node_tag",
-    "node_comment",
-    "organization",
-    "organization_contact",
-    "organization_endpoint",
-    "organization_machine_tag",
-    "organization_tag",
-    "organization_identifier",
-    "organization_comment",
-    "installation",
-    "installation_contact",
-    "installation_endpoint",
-    "installation_machine_tag",
-    "installation_tag",
-    "installation_comment",
-    "dataset",
-    "dataset_contact",
-    "dataset_endpoint",
-    "dataset_machine_tag",
-    "dataset_tag",
-    "dataset_identifier",
-    "dataset_comment",
-    "dataset_network",
-    "network",
-    "network_contact",
-    "network_endpoint",
-    "network_machine_tag",
-    "network_tag",
-    "network_comment",
-    "contact",
-    "endpoint",
-    "endpoint_machine_tag",
-    "machine_tag",
-    "tag",
-    "identifier",
-    "comment",
-    "metadata"
-    );
-
   public DatabaseInitializer(DataSource dataSource) {
     this.dataSource = dataSource;
   }
@@ -93,10 +46,16 @@ public class DatabaseInitializer extends ExternalResource {
     Connection connection = dataSource.getConnection();
     try {
       connection.setAutoCommit(true);
-      for (String table : TABLES) {
-        connection.createStatement().execute("DELETE FROM TABLE " + table + " CASCADE");
-      }
-
+      connection.createStatement().execute("TRUNCATE TABLE " +
+        "node, node_contact, node_machine_tag, node_tag, node_comment, " +
+        "organization, organization_contact, organization_endpoint, organization_machine_tag, " +
+        "organization_tag, organization_identifier, organization_comment, " +
+        "installation, installation_contact, installation_endpoint, installation_machine_tag, " +
+        "installation_tag, installation_comment, " +
+        "dataset, dataset_contact, dataset_endpoint, dataset_machine_tag, dataset_tag, " +
+        "dataset_identifier, dataset_comment, " +
+        "network, network_contact, network_endpoint, network_machine_tag, network_tag, network_comment, " +
+        "contact, endpoint, machine_tag, tag, identifier, comment CASCADE");
     } catch (SQLException e) {
       Throwables.propagate(e);
     } finally {
