fa638023831999e9917a7183cdb981c3c6ce1917
timrobertson100 <timrobertson100@gmail.com>
2013-04-17 13:45:18 +0000

    Fixed for staging environment (need to rethink this though)

--- registry2/trunk/registry-ws/src/main/java/org/gbif/registry2/ws/servlet/StaticContentFilter.java
+++ registry2/trunk/registry-ws/src/main/java/org/gbif/registry2/ws/servlet/StaticContentFilter.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2013 Global Biodiversity Information Facility (GBIF)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,7 +39,7 @@ public class StaticContentFilter implements Filter {
     HttpServletRequest req = (HttpServletRequest) request;
     String path = req.getRequestURI().substring(req.getContextPath().length());
 
-    if (path.contains("/web/") || path.contains("/web2/")) {
+    if (path.contains("web") || path.contains("web2")) {
       // do not chain any more filters
       request.getRequestDispatcher(req.getRequestURI()).forward(request, response);
 
