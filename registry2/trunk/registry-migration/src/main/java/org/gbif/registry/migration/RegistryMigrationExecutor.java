2297b41a027f91809faf5f8fe21ca88663e3fdf3
lars.francke@gmail.com <lars.francke@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-04-18 07:55:53 +0000

    Cleaning up POM, improve logging output, fix problem where functions can't be created in PostgreSQL

--- registry2/trunk/registry-migration/src/main/java/org/gbif/registry/migration/RegistryMigrationExecutor.java
+++ registry2/trunk/registry-migration/src/main/java/org/gbif/registry/migration/RegistryMigrationExecutor.java
@@ -2,6 +2,7 @@ package org.gbif.registry.migration;
 
 import java.io.File;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import scriptella.execution.EtlExecutor;
 import scriptella.execution.EtlExecutorException;
 import scriptella.execution.ExecutionStatistics;
@@ -10,18 +11,16 @@ import scriptella.interactive.ConsoleProgressIndicator;
 public class RegistryMigrationExecutor {
 
   private static ExecutionStatistics execute(String scriptFile, String consoleIndicator) throws EtlExecutorException {
-    ExecutionStatistics statics;
-    statics = EtlExecutor.newExecutor(new File(scriptFile)).execute(new ConsoleProgressIndicator(consoleIndicator));
-    return statics;
+    return EtlExecutor.newExecutor(new File(scriptFile)).execute(new ConsoleProgressIndicator(consoleIndicator));
   }
 
   public static void main(String[] args) {
     try {
-
-      ExecutionStatistics statics = null;
+      SLF4JBridgeHandler.removeHandlersForRootLogger();
+      SLF4JBridgeHandler.install();
 
       System.out.println("Starting migration...");
-      statics = execute("src/main/resources/startup.xml", "Registry node");
+      ExecutionStatistics statics = execute("src/main/resources/startup.xml", "Registry node");
 
       System.out.println("Starting nodes creation...");
       statics = execute("src/main/resources/migrate-nodes.xml", "Registry node");
@@ -38,35 +37,35 @@ public class RegistryMigrationExecutor {
       System.out.println("Starting datasets creation...");
       statics = execute("src/main/resources/migrate-datasets.xml", "Registry dataset");
       System.out.println("Datasets created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting networks creation...");
       statics = execute("src/main/resources/migrate-networks.xml", "Registry network");
       System.out.println("Networks created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting dataset_networks creation...");
       statics = execute("src/main/resources/migrate-dataset_networks.xml", "Registry dataset_network");
       System.out.println("Dataset_networks created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting contacts creation...");
       statics = execute("src/main/resources/migrate-contacts.xml", "Registry contact");
       System.out.println("Contacts created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting node_contacts creation...");
       statics = execute("src/main/resources/migrate-node_contacts.xml", "Registry node_contact");
       System.out.println("Node_contacts created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting organization_contacts creation...");
       statics = execute("src/main/resources/migrate-organization_contacts.xml", "Registry organization_contact");
       System.out.println("Organization_contacts created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting dataset_contacts creation...");
       statics = execute("src/main/resources/migrate-dataset_contacts.xml", "Registry dataset_contact");
       System.out.println("Dataset_contacts created in " + statics.getTotalTime() + " milliseconds");
-      
+
       System.out.println("Starting installation_contacts creation...");
       statics = execute("src/main/resources/migrate-installation_contacts.xml", "Registry installation_contact");
       System.out.println("Installation_contacts created in " + statics.getTotalTime() + " milliseconds");
-            
+
       System.out.println("Starting network_contacts creation...");
       statics = execute("src/main/resources/migrate-network_contacts.xml", "Registry network_contact");
       System.out.println("Network_contacts created in " + statics.getTotalTime() + " milliseconds");
@@ -74,9 +73,10 @@ public class RegistryMigrationExecutor {
       System.out.println("Starting endpoints creation...");
       statics = execute("src/main/resources/migrate-endpoints.xml", "Registry endpoint");
       System.out.println("Endpoints created in " + statics.getTotalTime() + " milliseconds");
-      
+
     } catch (EtlExecutorException e) {
       e.printStackTrace();
     }
   }
+
 }
