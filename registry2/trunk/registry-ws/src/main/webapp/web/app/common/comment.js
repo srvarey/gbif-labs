1aa91305e37dbca21f0bee579fc5d3727932ef56
timrobertson100 <timrobertson100@gmail.com>
2013-05-19 18:42:21 +0000

    - Adds contact support to node in console - Improved side menu behavior in console for node - Improves the forms for creating new identifiers, tags

---
+++ registry2/trunk/registry-ws/src/main/webapp/web/app/common/comment.js
@@ -0,0 +1,45 @@
+angular.module('comment', ['services.notifications'])
+
+.controller('CommentCtrl', function ($scope, $state, $stateParams, $resource, notifications) {
+  var Comment = $resource('../:type/:key/comment/:commentKey', {
+    type : $stateParams.type,
+    key : $stateParams.key,  
+    commentKey : '@id'}
+  );
+  
+  // loads the comments, and updates the scope
+  var refreshScope = function() {
+    Comment.query(function(data) {
+      $scope.comments = data;
+      $scope.counts.comment = data.length; // update parent counts
+    });
+  }
+  
+  refreshScope();
+
+  $scope.save = function(item) {
+    item.createdBy = "TODO: security for comment.js";
+    item.modifiedBy = "TODO: security for comment.js";
+    Comment.save(item,
+      function() {
+        notifications.pushForCurrentRoute("Comment successfully updated", 'info');
+        refreshScope();
+        $scope.editing = false; // close the form
+      },
+      function(response) {
+        notifications.pushForCurrentRoute(response.data, 'error');
+      });  
+  }
+  
+  $scope.delete = function(comment) {
+    Comment.delete({commentKey : comment.key},
+      function() {
+        notifications.pushForCurrentRoute("Comment successfully deleted", 'info');
+        refreshScope();
+        $scope.editing = false; // close the form
+      },
+      function(response) {
+        notifications.pushForCurrentRoute(response.data, 'error');
+      });
+  }
+});
