f6eba98b4df885b76e27a62ef2b5f7b7b0c48d68
wixner@gmail.com <wixner@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-06-26 16:36:53 +0000

    Remove unused NETWORK_ORIGIN search parameter

--- registry2/trunk/registry-api/src/main/java/org/gbif/api/model/registry2/search/DatasetSearchRequest.java
+++ registry2/trunk/registry-api/src/main/java/org/gbif/api/model/registry2/search/DatasetSearchRequest.java
@@ -87,10 +87,6 @@ public class DatasetSearchRequest extends FacetedSearchRequest<DatasetSearchPara
     addParameter(DatasetSearchParameter.KEYWORD, keyword.toString());
   }
 
-  public void addNetworkOfOriginFilter(UUID networkOfOriginKey) {
-    addParameter(DatasetSearchParameter.NETWORK_ORIGIN, networkOfOriginKey.toString());
-  }
-
   public void addOwningOrgFilter(UUID orgKey) {
     addParameter(DatasetSearchParameter.OWNING_ORG, orgKey.toString());
   }
