cb1c6e05ff11455b169e4f65f68d41cddf41e386
wixner <wixner@gmail.com>
2013-06-26 16:36:53 +0000

    Remove unused NETWORK_ORIGIN search parameter

--- registry2/trunk/registry-api/src/main/java/org/gbif/api/model/registry2/search/DatasetSearchParameter.java
+++ registry2/trunk/registry-api/src/main/java/org/gbif/api/model/registry2/search/DatasetSearchParameter.java
@@ -29,11 +29,6 @@ import java.util.UUID;
 public enum DatasetSearchParameter implements SearchParameter {
 
   /**
-   * The key of the network of origin.
-   */
-  NETWORK_ORIGIN(UUID.class),
-
-  /**
    * {@link org.gbif.api.vocabulary.registry2.DatasetType} enumeration value.
    */
   TYPE(DatasetType.class),
