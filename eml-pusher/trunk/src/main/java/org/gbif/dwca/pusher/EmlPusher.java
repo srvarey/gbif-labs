8c9b3326e94981246c1685be2ea7ac254185e0f7
wixner@gmail.com <wixner@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-06-28 16:48:31 +0000

    Adding simple tool to push all metadata documents from a crawler folder with dwcas into the registry

---
+++ eml-pusher/trunk/src/main/java/org/gbif/dwca/pusher/EmlPusher.java
@@ -0,0 +1,120 @@
+package org.gbif.dwca.pusher;
+
+import org.gbif.api.service.registry2.DatasetService;
+import org.gbif.dwc.text.Archive;
+import org.gbif.dwc.text.ArchiveFactory;
+import org.gbif.dwc.text.UnsupportedArchiveException;
+import org.gbif.registry2.ws.client.guice.RegistryWsClientModule;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.UUID;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A utility that will inspect a crawling filesystem and push all metadata documents found into the registry.
+ */
+public class EmlPusher {
+
+  private static final Logger LOG = LoggerFactory.getLogger(EmlPusher.class);
+  private static String dwcaSuffix = ".dwca";
+  private int pushCounter;
+  private int failCounter;
+  private DatasetService datasetService;
+
+  public void start(File rootDirectory, String registryUrl) throws UnsupportedArchiveException, IOException {
+    initClients(registryUrl);
+
+    pushCounter = 0;
+    File[] archiveFiles = findArchives(rootDirectory);
+    LOG.info("Found {} archives", archiveFiles.length);
+
+    for (File f : archiveFiles) {
+      push(f);
+    }
+    LOG.info("Done. {} metadata documents from {} archives pushed to registry, {} failed", pushCounter, archiveFiles.length, failCounter);
+  }
+
+  private void initClients(String registryUrl) {
+    Properties p = new Properties();
+    p.put("registry.ws.url", registryUrl);
+    Injector inj = Guice.createInjector(new RegistryWsClientModule(p));
+    datasetService = inj.getInstance(DatasetService.class);
+  }
+
+  private void push(File archiveFile) {
+    UUID key = getDatasetKey(archiveFile);
+    try {
+      Archive arch = open(archiveFile);
+      File eml = arch.getMetadataLocationFile();
+      if (eml != null && eml.exists()) {
+        InputStream in = new FileInputStream(eml);
+        try {
+          datasetService.insertMetadata(key, in);
+          LOG.info("Pushed metadata document for dataset {} into registry", key);
+          pushCounter++;
+        } finally {
+          in.close();
+        }
+      }
+    } catch (UnsupportedArchiveException e) {
+      LOG.warn("Skipping archive {} because of error[{}]", key, e.getMessage());
+      failCounter++;
+
+    } catch (Exception e) {
+      LOG.error("Unexpected exception when pushing metadata for dataset {}: {}", key, e);
+      failCounter++;
+    }
+  }
+
+  private UUID getDatasetKey(File archiveFile) {
+    File dir = archiveDir(archiveFile);
+    return UUID.fromString(dir.getName());
+  }
+
+  private File archiveDir(File archiveFile) {
+    return new File(archiveFile.getParentFile(), FilenameUtils.getBaseName(archiveFile.getName()));
+  }
+
+  private Archive open(File archiveFile) throws UnsupportedArchiveException {
+    // does the folder already exist (it should have the same name as the archive)?
+    File dir = archiveDir(archiveFile);
+    try {
+      if (dir.exists()) {
+        return ArchiveFactory.openArchive(dir);
+      } else {
+        LOG.info("Decompress archive {}", archiveFile.getAbsoluteFile());
+        return ArchiveFactory.openArchive(archiveFile, dir);
+      }
+    } catch (IOException e) {
+      throw new UnsupportedArchiveException(e);
+    }
+  }
+
+  // gets the list of archives
+  private File[] findArchives(File rootDirectory) {
+    return rootDirectory.listFiles((FileFilter) new SuffixFileFilter(dwcaSuffix, IOCase.INSENSITIVE));
+  }
+
+  public static void main(String[] args) {
+    EmlPusher app = new EmlPusher();
+    try {
+      app.start(new File(args[0]), args[1]);
+
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+    }
+  }
+}
