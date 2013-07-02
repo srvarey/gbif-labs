0aa7ea1bf3ab148c9a530f5b4eaab57b65af1605
timrobertson100@gmail.com <timrobertson100@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-07-02 20:03:14 +0000

    Optimizing the database truncation, and removed potential race condition between updating and decrementing the queue size.

--- registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
+++ registry2/trunk/registry-ws/src/test/java/org/gbif/registry2/database/DatabaseInitializer.java
@@ -45,17 +45,61 @@ public class DatabaseInitializer extends ExternalResource {
     LOG.info("Truncating registry tables");
     Connection connection = dataSource.getConnection();
     try {
-      connection.setAutoCommit(true);
-      connection.createStatement().execute("TRUNCATE TABLE " +
-        "node, node_identifier, node_machine_tag, node_tag, node_comment, " +
-        "organization, organization_contact, organization_endpoint, organization_machine_tag, " +
-        "organization_tag, organization_identifier, organization_comment, " +
-        "installation, installation_contact, installation_endpoint, installation_machine_tag, " +
-        "installation_tag, installation_comment, " +
-        "dataset, dataset_contact, dataset_endpoint, dataset_machine_tag, dataset_tag, " +
-        "dataset_identifier, dataset_comment, " +
-        "network, network_contact, network_endpoint, network_machine_tag, network_tag, network_comment, " +
-        "contact, endpoint, machine_tag, tag, identifier, comment, metadata CASCADE");
+      connection.setAutoCommit(false);
+      /*
+       * connection.createStatement().execute("TRUNCATE TABLE " +
+       * "node, node_identifier, node_machine_tag, node_tag, node_comment, " +
+       * "organization, organization_contact, organization_endpoint, organization_machine_tag, " +
+       * "organization_tag, organization_identifier, organization_comment, " +
+       * "installation, installation_contact, installation_endpoint, installation_machine_tag, " +
+       * "installation_tag, installation_comment, " +
+       * "dataset, dataset_contact, dataset_endpoint, dataset_machine_tag, dataset_tag, " +
+       * "dataset_identifier, dataset_comment, " +
+       * "network, network_contact, network_endpoint, network_machine_tag, network_tag, network_comment, " +
+       * "contact, endpoint, machine_tag, tag, identifier, comment, METADATA CASCADE");
+       */
+
+      // Profiling on laptop shows this to be far quicker, but above left it for easier reverting
+      connection.prepareStatement("DELETE FROM contact").execute();
+      connection.prepareStatement("DELETE FROM endpoint").execute();
+      connection.prepareStatement("DELETE FROM tag").execute();
+      connection.prepareStatement("DELETE FROM identifier").execute();
+      connection.prepareStatement("DELETE FROM comment").execute();
+      connection.prepareStatement("DELETE FROM node_identifier").execute();
+      connection.prepareStatement("DELETE FROM node_machine_tag").execute();
+      connection.prepareStatement("DELETE FROM node_tag").execute();
+      connection.prepareStatement("DELETE FROM node_comment").execute();
+      connection.prepareStatement("DELETE FROM organization_contact").execute();
+      connection.prepareStatement("DELETE FROM organization_endpoint").execute();
+      connection.prepareStatement("DELETE FROM organization_machine_tag").execute();
+      connection.prepareStatement("DELETE FROM organization_tag").execute();
+      connection.prepareStatement("DELETE FROM organization_identifier").execute();
+      connection.prepareStatement("DELETE FROM organization_comment").execute();
+      connection.prepareStatement("DELETE FROM installation_contact").execute();
+      connection.prepareStatement("DELETE FROM installation_endpoint").execute();
+      connection.prepareStatement("DELETE FROM installation_machine_tag").execute();
+      connection.prepareStatement("DELETE FROM installation_tag").execute();
+      connection.prepareStatement("DELETE FROM installation_comment").execute();
+      connection.prepareStatement("DELETE FROM dataset_contact").execute();
+      connection.prepareStatement("DELETE FROM dataset_endpoint").execute();
+      connection.prepareStatement("DELETE FROM dataset_machine_tag").execute();
+      connection.prepareStatement("DELETE FROM dataset_tag").execute();
+      connection.prepareStatement("DELETE FROM dataset_identifier").execute();
+      connection.prepareStatement("DELETE FROM dataset_comment").execute();
+      connection.prepareStatement("DELETE FROM network_contact").execute();
+      connection.prepareStatement("DELETE FROM network_endpoint").execute();
+      connection.prepareStatement("DELETE FROM network_machine_tag").execute();
+      connection.prepareStatement("DELETE FROM network_tag").execute();
+      connection.prepareStatement("DELETE FROM network_comment").execute();
+      connection.prepareStatement("DELETE FROM machine_tag").execute();
+      connection.prepareStatement("DELETE FROM metadata").execute();
+      connection.prepareStatement("DELETE FROM network").execute();
+      connection.prepareStatement("DELETE FROM dataset").execute();
+      connection.prepareStatement("DELETE FROM installation").execute();
+      connection.prepareStatement("DELETE FROM organization").execute();
+      connection.prepareStatement("DELETE FROM node").execute();
+      connection.commit();
+
     } catch (SQLException e) {
       Throwables.propagate(e);
     } finally {
