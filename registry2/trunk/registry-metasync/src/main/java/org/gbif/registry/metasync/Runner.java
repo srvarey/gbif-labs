628a694532c1899317e1f99b3cfbe38ccb52812e
timrobertson100@gmail.com <timrobertson100@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-07-17 08:17:16 +0000

    Slight changes to the counters for readability

--- registry2/trunk/registry-metasync/src/main/java/org/gbif/registry/metasync/Runner.java
+++ registry2/trunk/registry-metasync/src/main/java/org/gbif/registry/metasync/Runner.java
@@ -14,9 +14,11 @@ package org.gbif.registry.metasync;
 
 import org.gbif.registry.metasync.api.MetadataSynchroniser;
 import org.gbif.registry.metasync.protocols.digir.DigirMetadataSynchroniser;
+import org.gbif.registry.metasync.protocols.tapir.TapirMetadataSynchroniser;
 import org.gbif.registry.metasync.util.HttpClientFactory;
 
 import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,14 +37,19 @@ public class Runner {
     MetadataSynchroniser synchroniser = new MetadataSynchroniserImpl();
 
     synchroniser.registerProtocolHandler(new DigirMetadataSynchroniser(clientFactory.provideHttpClient()));
-    // synchroniser.registerProtocolHandler(new TapirMetadataSynchroniser(clientFactory.provideHttpClient()));
+    synchroniser.registerProtocolHandler(new TapirMetadataSynchroniser(clientFactory.provideHttpClient()));
 
     // Mock TAPIR 7ee758ae-2548-4d22-8bc8-b10950a4bce9
-    // UUID uuid = UUID.fromString("7ee758ae-2548-4d22-8bc8-b10950a4bce9");
+    UUID uuid = UUID.fromString("7ee758ae-2548-4d22-8bc8-b10950a4bce9");
 
     // synchroniser.synchroniseInstallation(uuid);
     Context context = new Context();
-    synchroniser.synchroniseAllInstallations(1000, context);
+    try {
+      synchroniser.synchroniseAllInstallations(1000, context);
+      // synchroniser.synchroniseInstallation(uuid, context);
+    } catch (Exception e1) {
+      LOG.error(e1.getMessage(), e1);
+    }
     LOG.info("Finished synchronizing, counters follow:");
     for (Entry<String, Integer> e : context.getCounters().entrySet()) {
       LOG.info("{}: {}", e.getKey(), e.getValue());
