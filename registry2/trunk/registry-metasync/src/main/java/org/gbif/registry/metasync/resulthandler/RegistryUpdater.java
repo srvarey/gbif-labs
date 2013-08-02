3052c017259c9447ced4b9c89892775151929122
kyle.braak <kyle.braak@gmail.com>
2013-08-02 12:57:37 +0000

    Implements saveUpdatedDatasets(), with accompanying unit test that uses mock web service.  Work reviewed in http://dev.gbif.org/code/cru/CR-POR-138

--- registry2/trunk/registry-metasync/src/main/java/org/gbif/registry/metasync/resulthandler/RegistryUpdater.java
+++ registry2/trunk/registry-metasync/src/main/java/org/gbif/registry/metasync/resulthandler/RegistryUpdater.java
@@ -7,7 +7,9 @@ import org.gbif.api.model.registry2.Identifier;
 import org.gbif.api.model.registry2.MachineTag;
 import org.gbif.api.service.registry2.DatasetService;
 import org.gbif.api.vocabulary.registry2.DatasetType;
+import org.gbif.api.vocabulary.registry2.EndpointType;
 import org.gbif.registry.metasync.api.SyncResult;
+import org.gbif.registry.metasync.util.Constants;
 import org.gbif.registry2.ws.client.guice.RegistryWsClientModule;
 import org.gbif.ws.client.guice.GbifApplicationAuthModule;
 
@@ -15,6 +17,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.slf4j.Logger;
@@ -38,8 +41,18 @@ public class RegistryUpdater {
     GbifApplicationAuthModule auth = new GbifApplicationAuthModule(props);
     auth.setPrincipal("admin");
 
-    Injector injector = Guice.createInjector(new RegistryWsClientModule(props));
+    Injector injector = Guice.createInjector(new RegistryWsClientModule(props), auth);
     datasetService = injector.getInstance(DatasetService.class);
+  }
+
+  @VisibleForTesting
+  public RegistryUpdater(DatasetService datasetService) {
+    this.datasetService = datasetService;
+  }
+
+  @VisibleForTesting
+  public DatasetService getDatasetService() {
+    return datasetService;
   }
 
   public void saveSyncResultsToRegistry(Iterable<SyncResult> syncResults) {
@@ -68,19 +81,118 @@ public class RegistryUpdater {
     }
   }
 
-  private void saveUpdatedDatasets(SyncResult result) {
+  /**
+   * Iterate through the updated datasets. For each dataset, update the dataset itself, and then update its contacts,
+   * identifiers, machine tags, and endpoints. Tags remain unchanged by the metadata synchronizer, and therefore aren't
+   * updated. The following assumptions apply:
+   * </br>
+   * Contacts: are derived 100% from the metadata response.
+   * </br>
+   * Machine tags: with metasync.gbif.org namespace are derived 100% from metadata synchronizer
+   * </br>
+   * Endpoints: only 1 endpoint exists per dataset.
+   * </br>
+   * Identifiers:
+   *
+   * @param result SyncResult
+   */
+  void saveUpdatedDatasets(SyncResult result) {
     for (Map.Entry<Dataset, Dataset> entry : result.existingDatasets.entrySet()) {
       Dataset existingDataset = entry.getKey();
-      if (existingDataset.isLockedForAutoUpdate()) {
-        LOG.info("Dataset [{}] updated at source untouched in Registry because it's locked", existingDataset.getKey());
+      UUID datasetKey = existingDataset.getKey();
+      if (skipDatasetUpdate(existingDataset)) {
+        LOG.info("Dataset [{}] updated at source untouched in Registry because it's locked or contains a DwC-A "
+                 + "endpoint, which takes precedence", datasetKey);
       } else {
-        LOG.info("Updating dataset [{}]", existingDataset.getKey());
-        datasetService.update(existingDataset);
-      }
+        LOG.info("Updating dataset [{}]", datasetKey);
 
-      // TODO: Update the rest
-      // TODO: Map existing endpoints to new ones or rely on the synchroniser that it gave us proper stuff?
+        // the updated dataset created from the metadata synchronization
+        Dataset updated = entry.getValue();
+
+        // safest, preserve existing dataset, copying over only those properties that could have changed from the sync
+        existingDataset.setHomepage(updated.getHomepage());
+        existingDataset.setLanguage(updated.getLanguage());
+        existingDataset.setCitation(updated.getCitation());
+        existingDataset.setTitle(updated.getTitle());
+        existingDataset.setDescription(updated.getDescription());
+        existingDataset.setLogoUrl(updated.getLogoUrl());
+        existingDataset.setRights(updated.getRights());
+        // perform update
+        datasetService.update(existingDataset);
+
+        // Contacts are derived 100% from the metadata
+        // delete existing contacts, and replace with new/updated contacts
+        for (Contact contact: existingDataset.getContacts()) {
+          datasetService.deleteContact(datasetKey, contact.getKey());
+        }
+        for (Contact contact : updated.getContacts()) {
+          datasetService.addContact(datasetKey, contact);
+        }
+
+        // Machine tags with namepace "metasync.gbif.org" are derived 100% from the metadata sync
+        // delete existing machine tags in this namespace, and replace with new/updated machine tags
+        for (MachineTag machineTag: existingDataset.getMachineTags()) {
+          // TODO: replace below with datasetService.deleteMachineTags(uuid, namespace); once implemented
+          if (machineTag.getNamespace().equalsIgnoreCase(Constants.METADATA_NAMESPACE)) {
+            datasetService.deleteMachineTag(datasetKey, machineTag.getKey());
+          }
+        }
+        for (MachineTag machineTag : updated.getMachineTags()) {
+          datasetService.addMachineTag(datasetKey, machineTag);
+        }
+
+        // Only 1 endpoint exists per Dataset
+        // delete existing endpoint, and replace with new/updated endpoint
+        for (Endpoint endpoint: existingDataset.getEndpoints()) {
+          datasetService.deleteEndpoint(datasetKey, endpoint.getKey());
+        }
+        for (Endpoint endpoint : updated.getEndpoints()) {
+          datasetService.addEndpoint(datasetKey, endpoint);
+        }
+
+        // Only add identifiers that don't yet exist
+        // Identifiers like GBIF Portal ID aren't created during metadata sync, so never delete these
+        if (!updated.getIdentifiers().isEmpty()) {
+          for (Identifier identifier : updated.getIdentifiers()) {
+            // does this identifier exist already?
+            boolean found = false;
+            for (Identifier existingIdentifier: existingDataset.getIdentifiers()) {
+              if (identifier.lenientEquals(existingIdentifier)) {
+                found = true;
+                break;
+              }
+            }
+            if (!found) {
+              datasetService.addIdentifier(datasetKey, identifier);
+            }
+          }
+        }
+      }
     }
+  }
+
+  /**
+   * Check if the dataset update should be skipped. Currently, there are 2 reasons for skipping:
+   * 1) The dataset has been locked for automatic updates
+   * 2) The dataset has been migrated to DwC-A, in other words, it has an endpoint of type Darwin Core.
+   *
+   * @param existingDataset existing dataset (dataset as it currently exists in registry)
+   *
+   * @return true if the dataset update should be skipped, false otherwise
+   */
+  private boolean skipDatasetUpdate(Dataset existingDataset) {
+    // is the dataset locked for auto update?
+    if (existingDataset.isLockedForAutoUpdate()) {
+      return true;
+    }
+    // is the dataset migrated to DwC-A?
+    for (Endpoint endpoint : existingDataset.getEndpoints()) {
+      if (endpoint.getType() == EndpointType.DWC_ARCHIVE) {
+        return true;
+      }
+    }
+    // otherwise, proceed with update
+    return false;
   }
 
   private void saveDeletedDatasets(SyncResult result) {
