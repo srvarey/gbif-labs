6c899a1bd8bb7e196d9bf79cc6703d779c1eafe8
oliver.meyn@gmail.com <oliver.meyn@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-09-02 11:52:06 +0000

    up threads for dwca-validator from 1 to 3

--- crawler-config/trunk/bin/start-dwca-validator.sh
+++ crawler-config/trunk/bin/start-dwca-validator.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # NOTE: -Xmx should be threadcount * 1G to allow for worst case of 2M * 500byte triplets being held in memory
-nohup java -jar -Xms1G -Xmx1G ../lib/crawler-cli.jar validator --log-config ../config/logback-crawler-dwca-validator.xml --conf ../config/crawler-dwca-validator.yaml &> ../logs/crawler-dwca-validator_stdout.log &
+nohup java -jar -Xms3G -Xmx3G ../lib/crawler-cli.jar validator --log-config ../config/logback-crawler-dwca-validator.xml --conf ../config/crawler-dwca-validator.yaml &> ../logs/crawler-dwca-validator_stdout.log &
