ad04392478d17effbba1be7fac6d0151040eedeb
timrobertson100 <timrobertson100@gmail.com>
2013-09-02 15:28:44 +0000

    Adds CMS GC and GC logging

--- crawler-config/trunk/bin/start-cube-occurrence.sh
+++ crawler-config/trunk/bin/start-cube-occurrence.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-nohup java -Xms1G -Xmx1G -jar ../lib/metrics-cli.jar OccurrenceCube --log-config ../config/logback-cube-occurrence.xml --conf ../config/cube-occurrence.yaml &> ../logs/cube-occurrence_stdout.log &
+nohup java -Xms1G -Xmx1G -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=60 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=128M -jar ../lib/metrics-cli.jar OccurrenceCube --log-config ../config/logback-cube-occurrence.xml --conf ../config/cube-occurrence.yaml &> ../logs/cube-occurrence_stdout.log &
