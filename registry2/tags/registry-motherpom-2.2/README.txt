f01e77d9a98b3b3f3c0fe235d8d2c5153232d845
timrobertson100@gmail.com <timrobertson100@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-09-10 08:07:31 +0000

    [maven-release-plugin]  copy for tag registry-motherpom-2.2

---
+++ registry2/tags/registry-motherpom-2.2/README.txt
@@ -0,0 +1,4 @@
+Registry 
+--------
+
+THIS IS NOT THE LIVE REGISTRY (yet...)
---
+++ registry2/tags/registry-motherpom-2.2/registry-ws/src/main/webapp/web/app/app.js
@@ -0,0 +1,72 @@
+angular.module('app', [
+  'ui.compat', // the stateful angular router
+  'restangular', // for REST calls
+  'ngSanitize', // for the like	 of bind-html
+  'ngCookies', // for security
+  'http-auth-interceptor', // intercepts 403 responses, and triggers login
+  'services.notifications',
+  'home',
+  'login',
+  'search',
+  'organization',
+  'dataset',
+  'node',
+  'installation'
+  ])
+
+.config(['$routeProvider', 'RestangularProvider', '$httpProvider', function ($routeProvider, RestangularProvider, $httpProvider) {
+  // TODO: no idea, why angular starts up redirecting to #/index.html, but this adds a second redirect
+  $routeProvider.when('/index.html', {redirectTo: '/home'});
+  
+  // relative to /web brings us up to the root
+  // should this be run outside of the registry-ws project, this will need changed
+  RestangularProvider.setBaseUrl("../"); 
+    
+  // all GBIF entities use "key" and not "id" as the id, and this is used inn routing
+  RestangularProvider.setRestangularFields({
+    id: "key"
+  });  
+  
+  // we really do not want 401 responses (or the dreaded browser login window appears)
+  $httpProvider.defaults.headers.common['gbif-prefer-403-over-401']='true';
+}])
+
+// app constants are global in scope 
+.constant('DEFAULT_PAGE_SIZE', 50)
+
+.controller('AppCtrl', function ($scope, notifications, $state, $rootScope, notifications, $cookieStore, authService, Auth) {
+  // register global notifications once
+  $scope.notifications = notifications;
+
+  $scope.removeNotification = function (notification) {
+    notifications.remove(notification);
+  };
+  
+  $rootScope.$on('event:auth-loginRequired', function() {
+    notifications.pushForNextRoute("Requires account with administrative rights", 'error');
+    $rootScope.isLoggedIn = false;        
+    $state.transitionTo("login");
+  });
+    
+  $rootScope.logout = function() {
+    Auth.clearCredentials();
+  }
+  
+  $rootScope.isLoggedIn = authService.isLoggedIn(); // initialize with a default
+})
+
+// a safe array sizing filter  
+.filter('size', function() {
+  return function(input) {
+    if (input) {
+      return  _.size(input || {});
+    } else {
+      return 0;
+    }    
+  }
+})
+
+.run(['$rootScope', '$state', '$stateParams', function ($rootScope,   $state,   $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}]);
