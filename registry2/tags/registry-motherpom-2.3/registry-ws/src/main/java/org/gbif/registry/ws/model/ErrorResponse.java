2948050b7da9ff077ebf3c862d918919e04794d1
timrobertson100 <timrobertson100@gmail.com>
2013-09-10 18:21:02 +0000

    [maven-release-plugin]  copy for tag registry-motherpom-2.3

---
+++ registry2/tags/registry-motherpom-2.3/registry-ws/src/main/java/org/gbif/registry/ws/model/ErrorResponse.java
@@ -0,0 +1,31 @@
+package org.gbif.registry.ws.model;
+
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Class used to generate error response for legacy (GBRDS/IPT) API.
+ * </br>
+ * JAXB annotations allow the class to be converted into an XML document or JSON response. @XmlElement is used to
+ * specify element names that consumers of legacy services expect to find.
+ */
+@XmlRootElement(name = "error")
+public class ErrorResponse {
+
+  @XmlAttribute
+  private String error;
+
+  public ErrorResponse(String error) {
+    this.error = error;
+  }
+
+  @NotNull
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+}
