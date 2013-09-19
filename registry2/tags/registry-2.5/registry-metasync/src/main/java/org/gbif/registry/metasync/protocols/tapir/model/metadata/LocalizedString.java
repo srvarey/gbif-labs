4434e81dcc99520552a1c873f9a3dc40256ab27e
timrobertson100 <timrobertson100@gmail.com>
2013-09-19 09:20:17 +0000

    [maven-release-plugin]  copy for tag registry-2.5

---
+++ registry2/tags/registry-2.5/registry-metasync/src/main/java/org/gbif/registry/metasync/protocols/tapir/model/metadata/LocalizedString.java
@@ -0,0 +1,34 @@
+package org.gbif.registry.metasync.protocols.tapir.model.metadata;
+
+import org.gbif.api.vocabulary.Language;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+public class LocalizedString {
+
+  private final Map<Language, String> values = Maps.newHashMap();
+
+  public void addValue(Language language, String value) {
+    values.put(language, value);
+  }
+
+  public Map<Language, String> getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    if (values.isEmpty()) {
+      return "";
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (Map.Entry<Language, String> entry : values.entrySet()) {
+      sb.append(entry.getKey());
+      sb.append(entry.getValue());
+    }
+    return sb.toString();
+  }
+}
