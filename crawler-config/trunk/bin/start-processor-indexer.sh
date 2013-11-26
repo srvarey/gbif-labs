7565208a3a183c5524d9bf8cb8ddefdf69ece7b3
oliver.meyn@gmail.com <oliver.meyn@gmail.com@89ac5c38-9969-5761-5d00-0bba396465e7>
2013-11-26 12:25:50 +0000

    now two occ index updaters - cloud and jetty

--- crawler-config/trunk/bin/start-processor-indexer.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-nohup java -Xms256M -Xmx256M -jar ../lib/occurrence-cli.jar update-occurrence-index --conf ../config/processor-indexer.yaml --log-config ../config/logback-processor-indexer.xml &> ../logs/processor-indexer_stdout.log &
