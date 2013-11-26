110dd89e795863a9ecd1f96e52652dfed58416d7
oliver.meyn <oliver.meyn@gmail.com>
2013-11-26 12:25:31 +0000

    now two occ index updaters - cloud and jetty

--- crawler-config/trunk/bin/start-processors.sh
+++ crawler-config/trunk/bin/start-processors.sh
@@ -3,7 +3,8 @@
 ./start-processor-verbatim.sh
 ./start-processor-interpreted.sh
 sleep 1
-./start-processor-indexer.sh
+./start-processor-indexer-single.sh
+./start-processor-indexer-cloud.sh
 sleep 1
 ./start-occurrence-deleter.sh
 ./start-occurrence-mutator.sh
